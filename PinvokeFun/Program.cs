9aa1d80a6e41a7a325386d2ef28ebb2cb9675b23
Tom Dobbelaere <tom.dobbelaere@student.howest.be>
2018-05-22 09:07:08 +0200

    Add project files.

---
+++ PinvokeFun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PinvokeFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
